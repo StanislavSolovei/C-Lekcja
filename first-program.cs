2aec0b1505c2f67561b8fbb0a833300e0054b5d1
StanislavSolovei <edukacjateb176@gmail.com>
2025-11-29 16:44:17 +0100

    Add files via upload

---
+++ first-program.cs
@@ -0,0 +1,60 @@
+namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] table = { 5, 10, 15, 20, 25 };
+
+            int suma = 0;
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine(table[i]);
+            }
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                suma = table[i] + suma;
+
+                Console.WriteLine("Mnożenie liczb:" + table[i] * 2);
+            }
+
+            Console.WriteLine("Suma liczb:" + suma);
+
+
+            int max = table[0];
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] > max)
+                {
+                    max = table[i];
+                }
+            }
+            Console.WriteLine("Największa liczba w tablice to: " + max);
+
+            int min = table[0];
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] < min)
+                {
+                    min = table[i];
+                }
+            }
+            Console.WriteLine("Najmniejsza liczba w tablica to: " + min);
+
+
+            string[] name =  { "Karol", "Daria", "Ksawery", "Anna", "Ewa" };
+            
+            Console.WriteLine("Imiona w odwrotnej kolejności: ");
+            
+            for (int i = name.Length - 1; i >=0; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+
+            
+        }
+    }
+}
